43638a970de8c0a535bcc2d50fbb3e03cef7880d
Goncalo Marques <goncalomarques9@hotmail.com>
2021-02-02 10:23:55 +0000

    docker file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('How are you doing');
+  res.send('Hello there');
 });
 
 app.listen(8080, () => {
